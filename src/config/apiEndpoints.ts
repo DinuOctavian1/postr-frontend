9fe10c296d1db2867d486d3258755058120d1745
Octavian Dinu <dinuoctavianstefan@gmail.com>
2023-02-05 21:05:51 +0200

    Add apiEndpoints

---
+++ src/config/apiEndpoints.ts
@@ -0,0 +1,12 @@
+export const BASE_URL = 'https://localhost:44345';
+
+export const ENDPOINT = {
+    Register: '/api/auth/register',
+    Login: '/api/auth/login',
+    Logout: '/api/auth/logout',
+    GetUser: '/api/auth/user',
+    ConfirmEmail: '/api/auth/confirmemail',
+    ForgotPassword: '/api/auth/forgetpassword',
+    ResetPassword: '/api/auth/resetpassword',
+    Login2FA: '/api/auth/login/2fa',
+};
