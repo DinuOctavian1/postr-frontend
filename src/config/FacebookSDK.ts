da1c5eec2043e5afff03f2ede96082d1fbd7c488
Octavian Dinu <dinuoctavianstefan@gmail.com>
2023-05-23 11:46:46 +0300

    Fix Facebook sdk bug

--- src/config/FacebookSDK.ts
+++ src/config/FacebookSDK.ts
@@ -1,37 +1,44 @@
 import { useEffect, useState } from 'react';
 
+const loadFacebookSDK = () => {
+  return new Promise((resolve) => {
+    if (window.FB) {
+      // If the Facebook SDK is already loaded, resolve the Promise immediately
+      resolve(window.FB);
+    } else {
+      // Otherwise, wait for the asynchronous loading of the SDK
+      window.fbAsyncInit = () => {
+        window.FB.init({
+          appId: process.env.REACT_APP_FACEBOOK_APP_ID,
+          status: true,
+          xfbml: true,
+          version: 'v2.7',
+        });
+        resolve(window.FB);
+      };
+    }
+
+    // Load the Facebook SDK asynchronously
+    (function (d, s, id) {
+      const fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      const js: any = d.createElement(s);
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
+  });
+};
+
 const useFacebookSDK = () => {
   const [sdkLoaded, setSdkLoaded] = useState(false);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const loadFacebookSDK = () => {
-      return new Promise((resolve) => {
-        window.fbAsyncInit = () => {
-          window.FB.init({
-            appId: process.env.REACT_APP_FACEBOOK_APP_ID,
-            status: true,
-            xfbml: true,
-            version: 'v2.7',
-          });
-          resolve(window.FB);
-        };
-
-        // Load the Facebook SDK asynchronously
-        (function (d, s, id) {
-          const fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) {
-            return;
-          }
-          const js: any = d.createElement(s);
-          js.id = id;
-          js.src = 'https://connect.facebook.net/en_US/sdk.js';
-          fjs.parentNode.insertBefore(js, fjs);
-        })(document, 'script', 'facebook-jssdk');
-      });
-    };
-
     loadFacebookSDK().then((FB) => {
+      console.log('FB', FB);
       setSdkLoaded(true);
       setLoading(false);
     });
--- src/config/FacebookSDKWrapper.tsx
+++ src/config/FacebookSDKWrapper.tsx
@@ -1,15 +1,19 @@
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import useFacebookSDK from './FacebookSDK';
 
 const FacebookSDKWrapper = () => {
   const { sdkLoaded, loading } = useFacebookSDK();
 
+  useEffect(() => {
+    console.log('FacebookSDKWrapper');
+  }, [loading, sdkLoaded]);
+
   if (loading) {
     return <h1>Loading...</h1>;
   }
 
   if (!sdkLoaded) {
-    // You can customize the message or loading indicator here
     return <h1>Facebook SDK is still loading...</h1>;
   }
 
@@ -17,3 +21,6 @@ const FacebookSDKWrapper = () => {
 };
 
 export default FacebookSDKWrapper;
+// function useEfect(arg0: () => void, arg1: boolean[]) {
+//   throw new Error('Function not implemented.');
+// }
